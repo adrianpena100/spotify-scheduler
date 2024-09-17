af140153c063de2d63c2c5a4f3c174cd723968ff
ReneMorales777 <rene.morales01@utrgv.edu>
2024-09-17 16:24:49 -0500

    Modularity/Readability Check

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import "bootstrap/dist/css/bootstrap.min.css"
-import Login from "./Login"
-import Dashboard from "./Dashboard"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/App.css';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
 
 const code = new URLSearchParams(window.location.search).get("code")
 
